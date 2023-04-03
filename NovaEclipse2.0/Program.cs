dcd59b05519352b362d200dc6fd139d6b23bc1ff
Daniel Parks <adam.parks123@outlook.com>
2023-04-02 23:26:57 -0400

    Edited Program.cs to call CLASS menu. Gives instance of the menu, calles the menu and displays the menu's Display() method

---
+++ NovaEclipse2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+* 2 APR 2023
+* CSC 153
+* Daniel Parks
+* Nova Eclipse
+* 
+* SPRINT 4 completed. 
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using neClassLibrary1;
+
+namespace NovaEclipse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu menu = new Menu();
+            menu.Display();
+            
+        }
+    }
+}
